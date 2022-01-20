8a7c52385085b1f681b1bbf69bfdbdbd08187aaf
Thomas <thomas.sembely@gmail.com>
2022-01-20 20:15:52 +0100

    oh god...

---
+++ socket-backend/src/chat.gateway.ts
@@ -0,0 +1,11 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+export class ChatGateway {
+    @WebSocketServer()
+    server;
+    
+    @SubscribeMessage('message')
+    handleMessage(@MessageBody() message: string): void {
+        this.server.emit('message', message);
+    }
+}
