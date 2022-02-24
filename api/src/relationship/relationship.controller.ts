5daaa3356b96ab1cf80742b73d17a8b7f040e886
Adeline BENOIT <adbenoit@e1r4p10.42.fr>
2022-02-24 18:09:17 +0100

    relationship up

--- api/src/relationship/relationship.controller.ts
+++ api/src/relationship/relationship.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Param, Get, Res, Post, Request, Put, Query, UseGuards, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { RelationshipService } from 'src/relationship/relationship.service'
 @Controller('relationship')
-export class RelationshipController {}
+export class RelationshipController {
+    constructor(
+        private rsService: RelationshipService) {}
+
+    @Get(':u1/add/:u2')
+    async addFriend(@Param() params) {
+        this.rsService.addFriend(params.u1,params.u2);
+    }
+    
+    @Get(':u1/unfriend/:u2')
+    async unfriend(@Param() params) {
+        this.rsService.unfriend(params.u1, params.u2);
+    }
+    
+    @Get(':u1/accept/:u2')
+    async acceptFriendRequest(@Param() params) {
+        this.rsService.acceptFriendRequest(params.u1, params.u2);
+    }
+
+    @Get(':u1/refuse/:u2')
+    async refuseFriendRequest(@Param() params) {
+        this.rsService.refuseFriendRequest(params.u1, params.u2);
+    }
+
+    @Get(':u1/block/:u2')
+    async blockUser(@Param() params) {
+        this.rsService.blockUser(params.u1, params.u2);
+    }
+
+    @Get(':user/friendList')
+    async getFriendList(@Param() params) {
+        this.rsService.getFriendList(params.user);
+    }
+    @Get(':user/blockList')
+    async getBlockList(@Param() params) {
+        this.rsService.getBlockList(params.user);
+    }
+    @Get(':user/RequestList')
+    async getRequestList(@Param() params) {
+        this.rsService.getRequestList(params.user);
+    }
+}
--- api/src/relationship/relationship.service.ts
+++ api/src/relationship/relationship.service.ts
@@ -11,19 +11,28 @@ export class RelationshipService {
         private rsRepository: Repository<RelationshipEntity>)
         {}
 
-    async addFriend(newRelation: Irelationship, userId1: number, userId2: number) {
-        newRelation.userId1 = userId1;
-        newRelation.userId2 = userId2;
+    async addFriend(u1: number, u2: number) {
+        const newRelation = {
+            userId1: u1,
+            userId2: u2,
+            type: relationshipType.REQUEST
+        }
         await this.rsRepository.save(this.rsRepository.create(newRelation));
-        // return newRelation.id;
     }
+
     async unfriend(u1: number, u2: number) {
         await this.rsRepository.delete({userId1 : u1, userId2 :  u2});
         await this.rsRepository.delete({userId1 : u2, userId2 :  u1});
     }
     
     async blockUser(u1: number, u2: number) {
-        await this.rsRepository.update({userId1 : u1, userId2 :  u2}, { type: relationshipType.BLOCK });
+        await this.rsRepository.delete({userId1 : u2, userId2 :  u1});
+        const newRelation = {
+            userId1: u1,
+            userId2: u2,
+            type: relationshipType.BLOCK
+        }
+        await this.rsRepository.save(this.rsRepository.create(newRelation));
     }
     
     async acceptFriendRequest(u1: number, u2: number) {
@@ -34,6 +43,14 @@ export class RelationshipService {
         await this.rsRepository.delete({userId1 : u1, userId2 :  u2});
     }
 
+    async getFriendList(user: number) {
+    }
+
+    async getRequestList(user: number) {
+    }
+
+    async getBlockList(user: number) {
+    }
 }
 
 // SELECT DISTINCT userid1, userid2 FROM RelationShip WHERE userid1=1 OR userid2=1 AND type='friend';
