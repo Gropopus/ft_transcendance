7918e8676ae92a23beb93a98dfc6652be4ef0ef5
Adeline BENOIT <adbenoit@e1r12p3.42.fr>
2022-02-28 11:58:10 +0100

    rsModule++

--- api/src/relationship/relationship.module.ts
+++ api/src/relationship/relationship.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RelationshipController } from './relationship.controller';
 import { RelationshipService } from './relationship.service';
+import { RelationshipEntity } from './relationship.Entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([RelationshipEntity])
+  ],
   controllers: [RelationshipController],
   providers: [RelationshipService]
 })
