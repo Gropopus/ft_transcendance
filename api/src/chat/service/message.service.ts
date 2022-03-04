9969c48012de49fe580d372f5d62b90bc2fd6283
Thomas SEMBELY <thsembel@e1r10p13.42.fr>
2022-03-04 14:46:06 +0100

    mult modif..Friends module added and chat fixed

--- api/src/chat/service/message.service.ts
+++ api/src/chat/service/message.service.ts
@@ -27,30 +27,6 @@ export class MessageService {
 
   async findMessagesForChannel(channel: Ichannel, user: Iuser, options: IPaginationOptions): Promise<Pagination<Imessage>> {
 	
-	/*const blocked = this.userRepository
-        .createQueryBuilder("u")
-        .leftJoin('u.sentFriendRequests', 'c')
-        .leftJoin('u.receivedFriendRequests', 'r')
-        .where("r.creator = :id")
-        .andWhere("r.status = 'blocked'")
-        .setParameters({ id : user.id })
-        .getMany();
-
- const blockedIds = (await blocked).map(b => b.id);
-	if (blockedIds.length != 0) {
-		// query for messages excluding blocked users
-		const query = this.messageRepository
-        .createQueryBuilder('message')
-        .leftJoin('message.channel', 'channel')
-        .leftJoinAndSelect('message.user', 'u')
-        .where('channel.id = :channelId', { channelId: channel.id })
-        .andWhere('u.id NOT IN (:...blockedIds)')
-        .setParameters({ blockedIds: blockedIds })
-        .orderBy('message.created_at', 'DESC');
-
-		return paginate(query, options);*/
-	//}
-
 	const query = this.messageRepository
 	.createQueryBuilder('message')
 	.leftJoin('message.channel', 'channel')
