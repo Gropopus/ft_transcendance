d12a60e1bb109745ecc09ca68f2e5defaf8803b4
Maris GASPA <gmaris@e2r10p12.clusters.42paris.fr>
2022-03-28 16:50:39 +0200

    game proggress

--- frontend/src/components/observePage.vue
+++ frontend/src/components/observePage.vue
@@ -1,18 +1,18 @@
 <template>
-    <ul>
-        <div class="observeArea">
-            <canvas id="canvasObs" width="640" height="500"></canvas>
-        </div>
-        <div class="userRight">
-            <p>Player Right recap : </p>
-        </div>
-        <div class="userLeft">
-            <p>Player Left recap : </p>
-        </div>
-    </ul>
+	<div>
+		<div class="ObserveArea">
+			<canvas id="canvasObs" width="640" height="500"></canvas>
+		</div>
+		<div class="UserRecap">
+			<p>Joueur 1 : <em id="player-score">0</em> - Joueur 2 : <em id="computer-score">0</em></p>
+		</div>
+		<div class="SocialRecap">
+			<p>Osef</p>
+		</div>
+	</div>
 </template>
 
-<script>
+<script lang="ts">
 import { load } from '../script/pong.js'
 	export default	{
 		props:	{
@@ -20,16 +20,13 @@ import { load } from '../script/pong.js'
 				type:	[Number, String],
 				default:	"0"
 			},
-			currentPage:	{
-				type:	[Number, String],
-				default:	"0"
-			}
 		},
 		mounted() {	
+			console.log('user id: ' + this.userId);
 			load(this.userId);
 		},
-		unmounted() {
-		}
+		// unmounted() {d
+		// }
 
 	}
 </script>
