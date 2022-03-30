ef9a40893b6cff2f3e884a06e67bf8c1c30b574d
Adeline BENOIT <adbenoit@e2r11p4.clusters.42paris.fr>
2022-03-30 18:45:01 +0200

    arrow implementation watch

--- frontend/src/components/watchPage.vue
+++ frontend/src/components/watchPage.vue
@@ -1,9 +1,12 @@
 <template>
 	<div class="watchPage">
-        <div class="selectArea">
+		<div class="changeButton">
+			<img src="/src/assets/arrow-whitedown.png" class="rotateimg90">
+		</div>
+        <div v-if="actualSelect" class="selectArea">
 			<div  v-if="actualSelect" class="gameInfo">
 				<div class="playerInfo">
-					<div class="picture">
+					<div class="userPicture">
 						<img :src="picture.left" />
 					</div>
 					<div class="textInfo">
@@ -13,7 +16,7 @@
 				</div>
 				<div class="vs"> <h1> VS </h1> </div>
 				<div class="playerInfo">
-					<div class="picture">
+					<div class="userPicture">
 						<img :src="picture.right" />
 					</div>
 					<div class="textInfo">
@@ -27,16 +30,8 @@
 			</div>
 			<div v-if="!actualSelect"> No game selected. </div>
         </div>
-		<div class="gameList">
-			<p>Live game :
-				<button @click="fectGameList()" >Refresh</button>
-			</p>
-            <ul id="v-for-object" class="gameList">
-                <li v-for="value in gameList">
-                    {{ value.player_left_id.username }} vs {{ value.player_right_id.username }}
-                    <button @click="Select(value)"> Select </button>
-                </li>
-            </ul>
+		<div class="changeButton">
+			<img src="/src/assets/arrow-white-up.png" class="rotateimg90">
 		</div>
 	</div>
 </template>
@@ -72,6 +67,7 @@ export default	defineComponent ({
 
 	created() {
 		console.log("created");
+		this.actualSelect = this.gameList[0];
 	},
 
 	methods: {
@@ -90,8 +86,9 @@ export default	defineComponent ({
 			var i = 0;
 			while (this.gameList[i])
 			{
-				this.gameListPlaying.push(	this.gameList[i].player_left_id.username + ' vs ' + 
-										this.gameList[i].player_right_id.username + ' room: ' + this.gameList[i].id);
+				this.gameListPlaying.push(
+					this.gameList[i].player_left_id.username + ' vs ' + 
+					this.gameList[i].player_right_id.username + ' room: ' + this.gameList[i].id);
 				++i;
 			}
 		},
@@ -144,6 +141,10 @@ export default	defineComponent ({
 
 .gameList
 {
+	margin-top: 4%;
+	margin-right: 4%;
+	width: 25%;
+	/* text-align: center; */
 	/* float:	right;
 	width:	25%;
 	min-height:	500px;
@@ -152,11 +153,15 @@ export default	defineComponent ({
 	margin-bottom:	min(22px); */
 }
 
+.gameList > h1 {
+	border-bottom: solid 2px white;
+}
+
 .gameInfo {
 	display: flex;
 	flex-direction: row;
 	width: 100%;
-	height: 90%;
+	height: 80%;
 	gap: 2%;
 }
 
@@ -164,11 +169,11 @@ export default	defineComponent ({
 	/* flex: 1 1 0; */
 	display: flex;
 	align-items: center;
-	margin-right: 2%;
-	margin-left: 2%;
 	margin-top: 2%;
-	width: 10%;
-	height: 75%;
+	width: 3%;
+	height: 65%;
+	margin-left: auto;
+	margin-right: auto;
 }
 
 .vs > h1 {
@@ -176,8 +181,6 @@ export default	defineComponent ({
 	width: 40%;
 	height: 20%;
 	text-align: center;
-	margin-left: auto;
-	margin-right: auto;
 	color: rgb(23,61,199);
 	font-size: 300%;
 	text-shadow: 2px 0 0 rgb(255, 228, 113), -2px 0 0 rgb(255, 228, 113), 0 2px 0 rgb(255, 228, 113), 0 -2px 0 rgb(255, 228, 113), 1px 1px rgb(255, 228, 113), -1px -1px 0 rgb(255, 228, 113), 1px -1px 0 rgb(255, 228, 113), -1px 1px 0 rgb(255, 228, 113);
@@ -188,35 +191,33 @@ export default	defineComponent ({
 	margin-right: 4%;
 	margin-left: 4%;
 	margin-top: 4%;
+	margin-bottom: 4%;
 	border: solid 3px white;
 	border-radius: 30px;
-	width: 30%;
-	height: 75%;
-	max-height: 500px;
-	max-width: 800px;
-	min-height: 100px;
-	min-width: 100px;
+	width: 35%;
+	height: 80%;
 	display: flex;
 	flex-direction: column;
 	box-shadow: rgba(0, 0, 0, 0.1) 0 2px 4px;
 }
 
-.playerInfo > .picture {
+.playerInfo > .userPicture {
 	height: 50%;
 	width: 70%;
 	display: flex;
+	width: calc(33.333% - 1rem);
 	align-items: center;
 	margin-right: auto;
 	margin-left: auto;
 }
 
-.playerInfo > .picture > img {
+.playerInfo > .userPicture > img {
 	/* display: block; */
-	object-fit: contain;
-	width:100%;
-	height:100%;
+	overflow: hidden;
+	object-fit:cover;
 	border: solid 3px rgb(255, 228, 113);
 	border-radius: 100px;
+	/* width: 200%; */
 }
 
 .textInfo {
@@ -233,10 +234,11 @@ export default	defineComponent ({
 
 
 .watchButton {
+	flex: 1 1 0;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	margin-bottom: auto;
+	margin-bottom: 10px;
 }
 
 .watchButton > p {
@@ -250,7 +252,7 @@ export default	defineComponent ({
 	cursor: pointer;
 	display: inline-block;
 	font-family: -apple-system,sans-serif;
-	font-size: 1.2rem;
+	font-size: 2rem;
 	font-weight: 700;
 	letter-spacing: -.24px;
 	margin: 0;
@@ -271,4 +273,18 @@ export default	defineComponent ({
 	transform: scale(1.125);
 }
 
+.gameName {
+	width: 100%;
+}
+.gameName:hover {
+	background:	var(--deep-blue-10);
+}
+
+.rotateimg90 {
+  -webkit-transform:rotate(90deg);
+  -moz-transform: rotate(90deg);
+  -ms-transform: rotate(90deg);
+  -o-transform: rotate(90deg);
+  transform: rotate(90deg);
+}
 </style>
