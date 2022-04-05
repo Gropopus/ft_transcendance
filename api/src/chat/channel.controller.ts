0c0c4e536f4b48d3047fe9800ac5fa3d90e252e6
Thomas SEMBELY <thsembel@e2r8p16.clusters.42paris.fr>
2022-04-05 19:36:20 +0200

    password update

--- api/src/chat/channel.controller.ts
+++ api/src/chat/channel.controller.ts
@@ -51,9 +51,11 @@ export class ChannelController {
 
 	@UseGuards(JwtAuthGuard)
 	@Post(':id/update-password')
-	async updatePassword(@Param() params, @Body() password: string, @Query('page') page: number = 1, @Query('limit') limit: number = 10){
+	async updatePassword(@Param() params, @Body() password, @Query('page') page: number = 1, @Query('limit') limit: number = 10){
 		const channel = await this.channelService.getChannelInfo(params.id, { page, limit, route: 'http://localhost:3000/api/:id/users'})
-		return this.channelService.updatePassword(channel.items[0], password);
+		const pass : string = password.password;
+		console.log(pass);
+		this.channelService.changePasswordChannel(channel.items[0], pass);
 	}
 
 	@Put('delete/:id')
